367387973e898fc37f255c09a0afcf6f985e38e5
dmitri1998171 <dmitri199817@gmail.com>
2021-12-03 22:43:03 +0700

    JMG-21 map generator complete

--- source/main.cpp
+++ source/main.cpp
@@ -5,8 +5,152 @@
 
 #include "wrapper/irrLib.hpp"
 
+#define ARR_SIZE 6
+#define BALL_SIZE 0.2
+#define TILE_SIZE 11
+
+enum map_tiles_enum {
+	STRAIGHT,
+	ANGLE,
+	HILL,
+	RISE_15,
+	RISE_30,
+	RISE_45,
+	HOLE 
+};
+
+void mapGenerator() {
+	std::string base_path = "media/Converted/";
+	std::string path;
+	int curr_tile = 0, prev_tile = 0, turn = 0;
+	f32 x = 0, y = 0, z = 0;
+	int currentTileArr[ARR_SIZE] = {STRAIGHT, RISE_45, STRAIGHT, STRAIGHT, STRAIGHT, HOLE};
+	vector3df currPosition, currRotation;
+	IMeshSceneNode* map_tile[ARR_SIZE];
+
+	for (int i = 0; i < ARR_SIZE; i++)
+		currentTileArr[i] = 0 + rand() % (sizeof(map_tiles_enum) - 2);
+
+	for (int i = 0; i < ARR_SIZE; i++) {
+		curr_tile = currentTileArr[i];
+
+		if(i == ARR_SIZE - 1) {
+			curr_tile = HOLE;
+			currentTileArr[i] = curr_tile;
+		}
+		
+		if(!i) {
+			curr_tile = STRAIGHT;
+			currentTileArr[i] = curr_tile;
+			currPosition = vector3df(0, 0, 0);
+		}
+		else {
+			if(turn > 0) {
+				x = map_tile[i - 1]->getPosition().X;
+
+				if(prev_tile == ANGLE || curr_tile == ANGLE || curr_tile == HOLE)
+					x += TILE_SIZE - (TILE_SIZE / 3);
+				else
+					x += TILE_SIZE;
+
+				currRotation = vector3df(0, 90, 0);
+			}
+			else if(turn < 0) {
+				x = map_tile[i - 1]->getPosition().X;
+
+				if(prev_tile == ANGLE || curr_tile == ANGLE || curr_tile == HOLE)
+					x -= TILE_SIZE - (TILE_SIZE / 3);
+				else
+					x -= TILE_SIZE;
+
+				currRotation = vector3df(0, 90 * turn, 0);
+			}
+			else {
+				z = map_tile[i - 1]->getPosition().Z;
+
+				if(prev_tile == ANGLE || curr_tile == ANGLE || curr_tile == HOLE)
+					z += TILE_SIZE - (TILE_SIZE / 3);
+				else
+					z += TILE_SIZE;
+			}
+		}
+
+		if(curr_tile == STRAIGHT) 
+			path.append(base_path + "Main/" + "straight.obj");
+			
+		if(curr_tile == HOLE) 
+			path.append(base_path + "Main/" + "hole.obj");
+
+		if(curr_tile == HILL) 
+			path.append(base_path + "Main/" + "hill.obj");
+
+		if(curr_tile == ANGLE) {
+			path.append(base_path + "Main/" + "angle.obj");
+
+			// Два поворота подряд
+			if(prev_tile == ANGLE) {
+				if(turn > 0)
+					x = (map_tile[i - 1]->getPosition().X + (TILE_SIZE / 2));
+				else if(turn < 0) 
+					x = (map_tile[i - 1]->getPosition().X + (TILE_SIZE / 2)) * (-1);
+				else
+					z = map_tile[i - 1]->getPosition().Z + (TILE_SIZE / 2);
+			}
+
+			if(0 + rand() % 2) {
+				turn--;
+				currRotation += vector3df(0, 90, 0);
+			}else {
+				turn++;
+			}
+			
+			if(turn > 1) { 
+				turn = 0;
+				currRotation += vector3df(0, 90, 0);
+			}
+			if(turn < -1) { 
+				turn = 0;
+				currRotation = vector3df(0, -90, 0);
+			}
+		}
+
+		currPosition = vector3df(x, y, z);
+
+		// условие подьема проверяются после установки текущ. позиции т.к задает высоту для след. эл-та
+
+		if(curr_tile == RISE_15) {
+			path.append(base_path + "Rise/" + "rise_15.obj");
+			y += 2.2;	// ?
+		}
+		
+		if(curr_tile == RISE_30) {
+			path.append(base_path + "Rise/" + "rise_30.obj");
+			y += 4.7;	// ?
+		}
+
+		if(curr_tile == RISE_45) {
+			path.append(base_path + "Rise/" + "rise_45.obj");
+			y += 8.2;	// ?
+		}
+
+		map_tile[i] = oLoadStatModel(path.c_str(), "", currPosition, false);
+		map_tile[i]->setRotation(currRotation);
+
+		currRotation = vector3df(0, 0, 0);
+		prev_tile = curr_tile;
+		path.clear();
+	}
+	
+	cout << endl;
+}
+
 int main() {
-	context.device = createDevice(EDT_SOFTWARE, dimension2d<u32>(600, 400), 16, false, false, false, &context.receiver);
+	int fps = 1;
+	int lastFPS = -1;
+
+	srand(time(0));
+	context.device = createDevice(EDT_SOFTWARE, dimension2d<u32>(800, 600), 16, false, false, false, &context.receiver);
+	u32 timeNow = context.device->getTimer()->getTime();
 
 	if (!context.device)
 		return 1; 
@@ -14,57 +158,40 @@ int main() {
 	context.driver = context.device->getVideoDriver();
 	context.smgr = context.device->getSceneManager();
 
-	context.smgr->getGUIEnvironment()->addStaticText(L"Press Space to hide occluder.", recti(10,10, 200,50));
+	// Map generator
+	mapGenerator();
+	// oLoadStatModel("media/Converted/Main/straight.obj", "", vector3df(0, 0, 0), false);
+	
+	// Ball and golf club
+	ISceneNode* ball = context.smgr->addSphereSceneNode(BALL_SIZE);
+	ball->setPosition(vector3df(0, 1, 0));
 
-	ISceneNode * node = context.smgr->addSphereSceneNode(10, 64);
-	if (node) {
-		node->setPosition(vector3df(0,0,60));
-		node->setMaterialTexture(0, context.driver->getTexture("media/wall.bmp"));
-		node->setMaterialFlag(EMF_LIGHTING, false);
-	}
+	// ISceneNode* golfClub = context.smgr->addCubeSceneNode(BALL_SIZE, 0, 0, vector3df(0, 0, -2));
+	// golfClub->setScale(vector3df(1, 5, 1));
 
-	ISceneNode* plane = context.smgr->addMeshSceneNode(context.smgr->addHillPlaneMesh(
-		"plane", dimension2df(10,10), dimension2du(2,2)), 0, -1,
-		vector3df(0,0,20), vector3df(270,0,0));
+	// Camera and cursor
+	ICameraSceneNode* camera = context.smgr->addCameraSceneNodeFPS();
+	// camera->setParent(ball);
+	camera->setPosition(ball->getPosition() + vector3df(35, 0, 0));
+	camera->setTarget(ball->getPosition());
 
-	if (plane) {
-		plane->setMaterialTexture(0, context.driver->getTexture("media/t351sml.jpg"));
-		plane->setMaterialFlag(EMF_LIGHTING, false);
-		plane->setMaterialFlag(EMF_BACK_FACE_CULLING, true);
-	}
-
-	context.driver->addOcclusionQuery(node, ((IMeshSceneNode*)node)->getMesh());
-
-	context.smgr->addCameraSceneNode();
-	int lastFPS = -1;
-	u32 timeNow = context.device->getTimer()->getTime();
-	bool nodeVisible=true;
+	context.device->getCursorControl()->setVisible(false);
 
 	while(context.device->run()) {
-		plane->setVisible(!context.receiver.IsKeyDown(KEY_SPACE));
-
 		context.driver->beginScene(true, true, SColor(255,113,113,133));
-	
-		node->setVisible(nodeVisible);
+		
 		context.smgr->drawAll();
 		context.smgr->getGUIEnvironment()->drawAll();
 
-		if (context.device->getTimer()->getTime()-timeNow>100) {
-			context.driver->runAllOcclusionQueries(false);
-			context.driver->updateAllOcclusionQueries();
-			nodeVisible=context.driver->getOcclusionQueryResult(node)>0;
-			timeNow=context.device->getTimer()->getTime();
-		}
-
 		context.driver->endScene();
 
-		int fps = context.driver->getFPS();
+		fps = context.driver->getFPS();
 
 		if (lastFPS != fps) {
-			stringw tmp(L"OcclusionQuery Example [");
-			tmp += context.driver->getName();
-			tmp += L"] fps: ";
+			stringw tmp(L"Just Mini Golf [");
+			tmp += L"fps: ";
 			tmp += fps;
+			tmp += L"]";
 
 			context.device->setWindowCaption(tmp.c_str());
 			lastFPS = fps;
@@ -72,7 +199,6 @@ int main() {
 	}
 
 	context.device->drop();
-	
 	return 0;
 }
 
