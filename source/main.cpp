c412e739ac75e5435793c2ee0b495f079d6e7dfe
dmitri1998171 <dmitri199817@gmail.com>
2021-12-13 12:49:19 +0300

    add road turn

--- source/main.cpp
+++ source/main.cpp
@@ -5,8 +5,9 @@
 
 #include "wrapper/irrLib.hpp"
 
-#define ARR_SIZE 10
+#define ARR_SIZE 6
 #define BALL_SIZE 0.2
+#define TILE_SIZE 11
 
 enum map_tiles_enum {
 	STRAIGHT,
@@ -15,30 +16,93 @@ enum map_tiles_enum {
 };
 
 void mapGenerator() {
-	int j = 1;
+	int curr_x = 0, curr_z = 0, turn = 0;
+
+	int currentTileArr[ARR_SIZE] = {0, 1, 0, 0, 0, 2};
+
 	int currentTile = 0;
-	int previousTile = 0;
-	vector3df currentVector;
+	int previousTile = -1;
+
+	char* path;
+	vector3df currPosition, currRotation;
 	IMeshSceneNode* map_tile[ARR_SIZE];
 
+	// for (int i = 0; i < ARR_SIZE; i++) {
+	// 	currentTile = 0 + rand() % (sizeof(map_tiles_enum) - 2);
+	// 	currentTileArr[i] = currentTile;
+	// 	cout << currentTile << endl;
+	// }
+
 	for (int i = 0; i < ARR_SIZE; i++) {
-		currentTile = 0 + rand() % 2;
-		
-		cout << "currentTile: " << currentTile << endl;
 
-		if(previousTile == ANGLE) {
-			currentVector = vector3df(j * 11, 0, ((i - 1) * 11));
-			j++;
+		currentTile = currentTileArr[i];
+
+		cout << "curr_x: " << curr_x << "  curr_z: " << curr_z << endl;
+
+		if(i == 0) 					// Первый элемент - всегда прямая
+			currentTile = STRAIGHT;
+
+		if (i == ARR_SIZE - 1) 		// Последний элемент - всегда лунка
+			currentTile = HOLE;
+
+		if(currentTile == STRAIGHT) {
+			path = "media/Converted/Main/straight.obj";
+			currPosition = vector3df(0, 0, (i * TILE_SIZE));
+
+			if(turn) {
+				currPosition = vector3df((curr_x * TILE_SIZE) - (TILE_SIZE / 3), 0, (curr_z * TILE_SIZE)  - (TILE_SIZE / 3));
+				currRotation = vector3df(0, 90, 0);
+			}
+			curr_x++;
 		}
-		else
-			currentVector = vector3df(0, 0, (i * 11));
 
-		if(currentTile == STRAIGHT)
-			map_tile[i] = oLoadStatModel("media/Converted/Main/straight.obj", "", currentVector, false);
+		if(currentTile == ANGLE) {
+			path = "media/Converted/Main/angle.obj";
+			currPosition = vector3df(0, 0, (i * TILE_SIZE) - (TILE_SIZE / 3));
+			curr_z++;
+
+			if(turn){
+				currPosition = vector3df((curr_x * TILE_SIZE) - (TILE_SIZE / 3), 0, (curr_z * TILE_SIZE) - (TILE_SIZE / 3));
+				turn = 0;
+			}
+
+			turn = 1;
+		}
+
+		if(currentTile == HOLE) {
+			path = "media/Converted/Main/hole.obj";
+
+			if(turn)
+				currPosition = vector3df((curr_x * TILE_SIZE) - (TILE_SIZE / 2) - 1, 0, (curr_z * TILE_SIZE)  - (TILE_SIZE / 3));
+			else
+				currPosition = vector3df(0, 0, (i * TILE_SIZE) - (TILE_SIZE / 3));
+		}
 		
-		if(currentTile == ANGLE)
-			map_tile[i] = oLoadStatModel("media/Converted/Main/angle.obj", "", vector3df(0, 0, (i * 11)), false);
-	
+		
+		// if(previousTile == ANGLE) {
+		// 	if(currentTile == ANGLE) {
+		// 		currPosition = vector3df((curr_z * TILE_SIZE) - (TILE_SIZE / 2), 0, ((i - 1) * TILE_SIZE) - (TILE_SIZE / 3));
+		// 		currRotation = vector3df(0, 180, 0);
+		// 		// turn = 0;
+		// 	}
+
+		// 	if(currentTile == STRAIGHT) {
+		// 		currPosition = vector3df((curr_z * TILE_SIZE) - (TILE_SIZE / 3), 0, ((i - 1) * TILE_SIZE) - (TILE_SIZE / 3));
+		// 		currRotation = vector3df(0, 90, 0);
+		// 	}
+
+		// 	if(currentTile == HOLE) {
+		// 		currPosition = vector3df(((curr_z - 1) * TILE_SIZE) - (TILE_SIZE / 2), 0, ((i - 1) * TILE_SIZE) - (TILE_SIZE - (TILE_SIZE / 5)));
+		// 	}
+
+		// curr_z++;
+		// 	turn = 1;
+		// }
+		// else 
+		// 	currRotation = vector3df(0, 0, 0);
+
+		map_tile[i] = oLoadStatModel(path, "", currPosition, false);
+		map_tile[i]->setRotation(currRotation);
 		previousTile = currentTile;
 	}
 }
@@ -71,7 +135,7 @@ int main() {
 	// Camera and cursor
 	ICameraSceneNode* camera = context.smgr->addCameraSceneNodeFPS();
 	// camera->setParent(ball);
-	camera->setPosition(ball->getPosition() + vector3df(0, 5, -15));
+	camera->setPosition(ball->getPosition() + vector3df(0, 50, -15));
 	camera->setTarget(ball->getPosition());
 
 	context.device->getCursorControl()->setVisible(false);
